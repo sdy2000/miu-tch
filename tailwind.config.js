87623c8fef70c288df5265b4eeaacc8c9346a418
sajjad <sajjad.darvish.yektayi@gmail.com>
2023-09-03 16:23:17 +0430

    Create repository for miutch project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {},
+      colors: {
+        'element-bg': '#eff6ff',
+        'color-y': '#fbbf24',
+      },
+    },
+  },
+  plugins: [],
+}
